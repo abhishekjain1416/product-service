3674cb14f2059412c306c308ce65dfd2c01b5c8b
Abhishek Jain <abhishekjain1416@gmail.com>
2023-11-29 17:30:50 +0530

    Refact: Add comments for better code readability

--- src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -21,25 +21,38 @@ public class ProductService {
     
     public void createProduct(ProductRequest productRequest){
 
-        // Instance of the product object
+        // Create a new instance of the product object using the Builder pattern
         Product product = Product.builder()
             .name(productRequest.getName())
             .description(productRequest.getDescription())
             .price(productRequest.getPrice())
             .build();
 
+        // Save the product to the repository
         productRepository.save(product);
+
+        // Log a message indicating that the product is saved
         log.info("Product {} is saved", product.getId());
     }
 
     public List<ProductResponse> getAllProducts(){
 
+        // Retrieve a list of all products from the repository
         List<Product> products = productRepository.findAll();
+
+        // Map each Product entity to a ProductResponse and collect the results into a List
         return products.stream().map(this::mapToProductResponse).toList();
 
     }
 
+    /**
+     * This private method maps a Product entity to a ProductResponse DTO.
+     * @param product
+     * @return
+     */
     private ProductResponse mapToProductResponse(Product product){
+
+        // Create and return a ProductResponse using the Builder pattern
         return ProductResponse.builder()
             .id(product.getId())
             .name(product.getName())
